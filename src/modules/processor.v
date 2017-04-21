0727e2f9b8c7d2287f8cb613361b6c05f07a093c
YAMAGUCHI HITOSHI <yamaguti80@gmail.com>
2017-04-21 11:07:45 +0900

    use register

--- src/modules/processor.v
+++ src/modules/processor.v
@@ -3,9 +3,9 @@ module processor(
   input  reset,
   input  exec,
   input  [15:0] m_q,
-  output [15:0] m_data,
-  output m_wren,
-  output [11:0] m_addr
+  output reg [15:0] m_data,
+  output reg m_wren,
+  output reg [11:0] m_addr
   );
   
   ////// Controller
@@ -19,70 +19,71 @@ module processor(
   
   ////// P1
   reg [15:0] pc;
+  reg wren;
   initial pc <= 0;
-
-  reg [15:0] inst;
-  
   always @(posedge phase_bus[0]) begin
     wren <= 0; // reset register
     m_addr <= pc;
     m_wren <= 0;
-    pc <= pc + 1;  
+    pc <= pc + 16'd1;  
   end
+  
   ////// P2
-  reg [15:0] rs, rd, sd;
-  wire [15:0] o_ar, o_br;
-  reg [15:0] ar, br;
-  
-  register register_(
-    .clock(phase_bus[1] or phase_bus[4]), .ra(rs), .rb(rd),
-    .wren(0), .data(mdr),
-    .ar(o_ar), .br(o_br));
-  
+  reg [4:0] rs, rd, op, sd;
   always @(posedge phase_bus[1]) begin
     // m_data[15:14] == 11
     rs <= m_data[13:11];
     rd <= m_data[13:11];
     op <= m_data[7:4];
     sd <= m_data[3:0];
-  end
-  ////// P3
-  reg dr;
-  wire o_dr;
-  reg [15:0] alu_res;
-  alu alu_(
-    .a(ar), .b(br), .op(op),
-    .res(alu_res))
-  
-  reg [15:0] shifter_res;
-  shifter shifter_(
-    .a(ar), .d(br), .op(shfter_op),
-    .res(shifter_res));
-  
-  p3_multiplexer(
-    .alu_res(alu_res), .shifter_res(shifter_res),
-    .dr(o_dr));
+  end  
 
+  wire [15:0] o_ar, o_br;
+  register register_(
+    .clock(phase_bus[1] | phase_bus[4]),
+    .ra(rs), .rb(rd),
+    .wren(0), .data(mdr),
+    .ar(o_ar), .br(o_br));
+
+  ////// P3
+  reg [15:0] ar, br;
   always @(posedge phase_bus[2]) begin
     ar <= o_ar;
     br <= o_br;
   end
+  
+  wire o_dr;
+  wire [15:0] alu_res;
+  alu alu_(
+    .a(ar), .b(br), .op(op),
+    .res(alu_res));
+  
+  wire [15:0] shifter_res;
+  shifter shifter_(
+    .a(ar), .d(br), .op(shfter_op),
+    .res(shifter_res));
+  
+  multiplexer multiplexer_p3(
+    .t(alu_res), .f(shifter_res),
+    .x(1));
+
   ////// P4
-  reg [15:0] mdr;
-  
-  wire [15:0] written;
-  p4_multiplexer(
-    mdr, dr,
-    written);
-  
+  reg dr;
   always @(posedge phase_bus[3]) begin
-      dr <= o_dr
+      dr <= o_dr;
   end
+
+  reg [15:0] mdr;
+  wire [15:0] written;
+  multiplexer multiplexer_p4(
+    .t(dr), .f(mdr),
+    .x(1)
+    );
+
   ////// P5
-  
   always @(posedge phase_bus[4]) begin
-      wren <= 1;
-      ar <=   
+    wren <= 1;
+    mdr <= m_data;
   end
-  
+
 endmodule
