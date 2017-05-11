fc95953e0f4a237a43913440106f6d35939603ec
YAMAGUCHI HITOSHI <yamaguti80@gmail.com>
2017-05-11 17:41:53 +0900

    WIP dup memory

--- src/hardware.v
+++ src/hardware.v
@@ -9,14 +9,25 @@ module hardware (
   output reg [7:0] oled
   );
   
-  wire [15:0] m_data, m_q;
-  wire [11:0] m_addr;
-  wire m_wren;
+  wire [15:0] ir_m_data, ir_m_q;
+  wire [11:0] ir_m_addr;
+  wire ir_m_wren;
+  
   reg s_clock;
-  ram_inc #("../../memories/test_output.mif") ram_inc_ (
-    .data(m_data), .wren(m_wren), .address(m_addr),
+  ram_inc #("../../memories/test_output.mif") ir_ram_inc_ (
+    .data(ir_m_data), .wren(ir_m_wren), .address(ir_m_addr),
     .clock(~clock),
-    .q(m_q)
+    .q(ir_m_q)
+  );
+  
+  wire [15:0] main_m_data, main_m_q;
+  wire [11:0] main_m_addr;
+  wire main_m_wren;
+  
+  ram_inc #("../../memories/test_output.mif") main_ram_inc_ (
+    .data(main_m_data), .wren(main_m_wren), .address(main_m_addr),
+    .clock(~clock),
+    .q(main_m_q)
   );
   
   wire exec;
@@ -31,7 +42,10 @@ module hardware (
   
   processor processor_ (
     .clock(clock), .reset(~n_reset), .exec(exec),
-    .m_q(m_q), .m_data(m_data), .m_rw(m_wren), .m_addr(m_addr),
+    .ir_m_q(ir_m_q), .ir_m_data(ir_m_data),
+    .ir_m_rw(ir_m_wren), .ir_m_addr(ir_m_addr),
+    .main_m_q(main_m_q), .main_m_data(main_m_data),
+    .main_m_rw(main_m_wren), .main_m_addr(main_m_addr),
     .inpval1(inpval1), .inpval2(inpval2),
     .outval1(outval1), .outval2(outval2), .outsel(outsel), .outdisplay(outdisplay));
   
