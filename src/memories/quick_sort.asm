2f01c2589098572a914134a904ad2a0fa5824150
trungdv <viettrungdang@gmail.com>
2017-06-01 16:13:10 +0900

    quicksort change

--- src/memories/quick_sort.asm
+++ src/memories/quick_sort.asm
@@ -2,11 +2,11 @@
 -- left 0..15
 -- right 16..31
 li 0 0
-li 2 4
-sll 2 8
+li 1 4
+sll 1 8
 -- addi 2 1
 -- $2 = 1024
-st 2 0 0
+st 1 0 0
 
 li 2 8
 sll 2 8
@@ -14,10 +14,34 @@ sll 2 8
 addi 2 -1
 st 2 128 0
 
+:bubble_sort
+mov 3 1
+li 7 0
+li 0 0
+
+-- r0: swapped?, r3: i
+:bbs_loop
+mov 4 3
+ld 5 0 4
+addi 3 1
+ld 6 0 3
+cmp 3 2
+be :end_bubble
+cmp 5 6
+ble :bbs_loop
+li 0 1
+st 5 0 3
+st 6 0 4
+:end_bubble
+
+cmp 0 7
+be :end
+
 li 0 1
 
 :partition
 
+-- if r0 < 0 end
 li 7 0
 cmp 0 7
 be :end
@@ -31,40 +55,50 @@ ld 2 128 0
 cmp 2 1
 ble :partition
 
--- $3 pivot value: $r3 = a[left]
-mov 4 2
-sub 4 1
-srl 4 1
-add 4 1
-ld 3 0 4
-ld 5 0 1
-st 3 0 1
-st 5 0 4
-
-
 -- $4 left value: $r4 = left + 1
 -- $5 right value: $r5 = right 
 mov 4 1
-mov 5 2
 addi 4 1
+cmp 4 2
+bne :choose_pivot
+ld 6 0 1
+ld 7 0 2
+cmp 6 7
+blt :partition
+st 6 0 2
+st 7 0 1
+b 0 :partition
+
+:choose_pivot
+mov 5 2
+
+-- $3 pivot value: $r3 = a[left]
+-- r6: pivot, r3 = a[pivot], r5 = a[left]
+mov 6 2
+sub 6 1
+srl 6 1
+add 6 1
+-- swap a[pivot] and a[left]
+ld 3 0 6
+ld 7 0 1
+st 3 0 1
+st 7 0 6
 
 -- $r6 = a[left + 1]
 -- $r4: i
 :find_left
 ld 6 0 4
-nop
 -- if a[i] > pivot
 cmp 3 6
 blt :find_right
--- if i > right
-cmp 2 4
-blt :find_right
+-- if i > j
+cmp 5 4
+blt :end_partition
 addi 4 1
 b 0 :find_left
 
 :find_right
 ld 7 0 5
-nop
 -- if a[j] < pivot
 cmp 7 3
 blt :swap
@@ -90,7 +124,7 @@ b 0 :find_left
 st 7 0 1
 st 3 0 5
 
--- recursion
+-- recursion (add to stack)
 
 addi 5 -1
 st 1 0 0
@@ -109,31 +143,12 @@ b 0 :partition
 li 2 4
 sll 2 8
 
-addi 2 1
 ld 6 0 2
-addi 2 1
+sll 2 1
+addi 2 -1
 ld 7 0 2
 out 6 7 0
 
-addi 2 1
-ld 6 0 2
-addi 2 1
-ld 7 0 2
-out 6 7 1
-
-addi 2 1
-ld 6 0 2
-addi 2 1
-ld 7 0 2
-out 6 7 2
-
-addi 2 1
-ld 6 0 2
-addi 2 1
-ld 7 0 2
-out 6 7 3
-
 --b 0 :printing
 
-nop
 hlt
---
+++ src/memories/test_alu.asm
@@ -0,0 +1,9 @@
+in 0 0
+in 1 1
+out 0 1 0
+mov 2 0
+add 2 1
+mov 3 0
+addi 3 3
+out 2 3 1
+hlt
